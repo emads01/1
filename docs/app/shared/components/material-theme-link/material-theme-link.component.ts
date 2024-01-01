f5dd306f13a3172f4653ac797914271b770a3fdc
emads01 <152613385+emads01@users.noreply.github.com>
2024-01-01 14:40:17 +0300

    Initialize update-angular-for-demo

---
+++ docs/app/shared/components/material-theme-link/material-theme-link.component.ts
@@ -0,0 +1,32 @@
+/**
+ * @license
+ * Copyright Akveo. All Rights Reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ */
+
+import { Component, ViewChild, AfterViewInit, Input } from '@angular/core';
+import { coerceBooleanProperty } from '@angular/cdk/coercion';
+import { NbPopoverDirective } from '@nebular/theme';
+
+@Component({
+  selector: 'ngx-material-theme-link',
+  templateUrl: './material-theme-link.component.html',
+  styleUrls: ['./material-theme-link.component.scss'],
+})
+export class MaterialThemeLinkComponent implements AfterViewInit {
+  public showPopover: boolean = false;
+
+  @Input() public set withPopover(value: any) {
+    this.showPopover = coerceBooleanProperty(value);
+  }
+
+  @ViewChild(NbPopoverDirective, { static: true }) public popover: NbPopoverDirective;
+
+  public ngAfterViewInit(): void {
+    this.showPopover && this.popover && this.popover.show();
+  }
+
+  public hidePopover(): void {
+    this.popover && this.popover.hide();
+  }
+}
